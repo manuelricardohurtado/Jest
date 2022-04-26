6999d80ea0c09f2e05fecb4497017f95b4e0e41c
Manuel Ricardo <manuelricardohurtado@gmail.com>
2022-04-26 15:39:42 -0400

    Async / Await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -32,3 +32,33 @@ test('Probar un Callback', () => {
     });
 });
 
+//Preubas con Promesas:
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    });
+};
+
+test('Probar una promesa', () => {
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH');
+        });
+});
+
+//Pruebas con Async & Await:
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('hola');
+    expect(string).toBe('aloh');
+});
+
+afterEach(() => console.log('Despues de casa prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
